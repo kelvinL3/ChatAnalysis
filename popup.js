c7af5438e88527a44c155d54b6a944e4a4c488e9
Kelvin <kelvinliu1234@gmail.com>
2017-09-09 04:14:36 -0400

    messages.htm

--- popup.js
+++
@@ -1,5 +0,0 @@
-else if(e.target.id=="import") {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(tabs[0].id, {file: "src/content_script.js"});
-  });
-}
