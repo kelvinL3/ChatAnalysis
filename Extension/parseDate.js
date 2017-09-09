7f24433161c72d83624ce6e5d1315e6fb2d37048
Kelvin <kelvinliu1234@gmail.com>
2017-09-09 06:42:38 -0400

    d

--- Extension/parseDate.js
+++ Extension/parseDate.js
@@ -1,5 +1,5 @@
 function parseDate(fucntion() {
 	//to be completed
 	//convert something like this "Sunday, April 16, 2017 at 8:59pm EDT"
-	//to an integer starting from some arbitrary date
+	//to an integer starting from some arbitrary date, as an integer where each step counts for a minute
 });
